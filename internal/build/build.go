69a6260472767fc8938cf7c7664557f06ec6ecbe
hasansino <4024450+hasansino@users.noreply.github.com>
2025-08-21 16:43:35 +0300

    fix: version info for go install

--- internal/build/build.go
+++ internal/build/build.go
@@ -1,14 +1,30 @@
 package build
 
+import (
+	"runtime/debug"
+)
+
 var (
 	xBuildCommit  = "dev"
 	xBuildVersion = "dev"
 )
 
 func GetCommit() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+	}
 	return xBuildCommit
 }
 
 func GetVersion() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "(devel)" && info.Main.Version != "" {
+			return info.Main.Version
+		}
+	}
 	return xBuildVersion
 }
